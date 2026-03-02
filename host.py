3083db79b09767097edb991a37ad4613a7edc115
Zhaofeng Yang (楊肇峰) <yangzhaofengsteven@gmail.com>
2026-03-02 11:47:03 +0800

    Update host.py

--- host.py
+++ host.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 import sys
 
 class InfiniteZeroHandler(BaseHTTPRequestHandler):
@@ -7,9 +7,18 @@ class InfiniteZeroHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-Type','application/octet-stream')
         self.end_headers()
-        while True:
-            self.wfile.write(b'\0' * 524288)
+        try:
+            while True:
+                self.wfile.write(b'\0' * 524288)
+        except BrokenPipeError:
+            pass
+        except Exception as e:
+            print("Connection error:", e, file=sys.stderr)
 
-server = HTTPServer(('0.0.0.0', 80), InfiniteZeroHandler)
+server = ThreadingHTTPServer(('0.0.0.0', 80), InfiniteZeroHandler)
 print("Serving infinite zeros on 0.0.0.0:80...")
-server.serve_forever()
+try:
+    server.serve_forever()
+except KeyboardInterrupt:
+    print("\nShutting down...")
+    server.server_close()
